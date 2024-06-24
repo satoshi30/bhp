c4628bbe46dc8425f88a64958a70c6f90cf90b7d
joh <joh@kali>
2024-06-23 21:21:56 -0400

    03

--- 03_raw_socket/sniffer_ip_header_parse_ctypes.py
+++ 03_raw_socket/sniffer_ip_header_parse_ctypes.py
@@ -1,6 +1,8 @@
 from ctypes import *
 import socket
 import struct
+import sys
+import os
 
 class IP(Structure):
 	_fields_ = [
@@ -24,4 +26,46 @@ class IP(Structure):
 		# IPアドレスを可読な形で変数に格納
 		self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
 		self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+
+		# プロトコルの定義値を名称にマッピング
+		self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+		try:
+			self.protocol = self.protocol_map[self.protocol_num]
+		except Exception as e:
+			print('%s No protocol for %s' % (e, self.protocol_num))
+			self.protocol = str(self.protocol_num)
+
 	
+def sniff(host):
+	if os.name == 'nt':
+		socket_protocol = socket.IPPROTO_IP
+	else:
+		socket_protocol = socket.IPPROTO_ICMP
+	
+	sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+	sniffer.bind((host, 0))
+	sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+	if os.name == 'nt':
+		sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+	
+	try:
+		while True:
+			# パケットの読み込み
+			raw_buffer = sniffer.recvfrom(65535)[0]
+			# バッファの最初の20バイトからIP構造体を作成
+			ip_header = IP(raw_buffer[0:20])
+			# 検出されたプロトコルとホストを出力
+			print('Protocol: %s %s -> %s' % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+	except KeyboardInterrupt:
+		# Windowsの場合はプロミスキャスモードを無効化
+		if os.name == 'nt':
+			sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+		sys.exit()
+
+if __name__ == '__main__':
+	if len(sys.argv) == 2:
+		host = sys.argv[1]
+	else:
+		host = '10.0.2.15'
+	sniff(host)

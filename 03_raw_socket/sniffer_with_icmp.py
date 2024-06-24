1f63de640eecbd5c968bc0069a627498c772a5b0
joh <joh@kali>
2024-06-24 11:07:09 -0400

    05

--- 03_raw_socket/sniffer_with_icmp.py
+++ 03_raw_socket/sniffer_with_icmp.py
@@ -30,7 +30,17 @@ class IP:
 		except Exception as e:
 			print('%s No protocol for %s' % (e, self.protocol_num))
 			self.protocol = str(self.protocol_num)
-	
+
+class ICMP:
+	def __init__(self, buff):
+		header = struct.unpack('<BBHHH', buff)
+		self.type = header[0]
+		self.code = header[1]
+		self.sum = header[2]
+		self.id = header[3]
+		self.seq = header[4]
+
+
 def sniff(host):
 	if os.name == 'nt':
 		socket_protocol = socket.IPPROTO_IP
@@ -50,8 +60,20 @@ def sniff(host):
 			raw_buffer = sniffer.recvfrom(65535)[0]
 			# バッファの最初の20バイトからIP構造体を作成
 			ip_header = IP(raw_buffer[0:20])
-			# 検出されたプロトコルとホストを出力
-			print('Protocol: %s %s -> %s' % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+			# ICMPであればそれを処理
+			if ip_header.protocol == "ICMP":
+				# 検出されたプロトコルとホストを出力
+				print('Protocol: %s %s -> %s' % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+				print(f'Version: {ip_header.ver}')
+				print(f'Header Length: {ip_header.ihl}')
+				print(f'TTL: {ip_header.ttl}')
+
+				# ICMPパケットの位置を計算
+				offset = ip_header.ihl * 4
+				buf = raw_buffer[offset:offset + 8]
+				# ICMP構造体を作成
+				icmp_header = ICMP(buf)
+				print('ICMP -> Type: %s Code: %s\n' % (icmp_header.type, icmp_header.code))
 	except KeyboardInterrupt:
 		# Windowsの場合はプロミスキャスモードを無効化
 		if os.name == 'nt':
---
+++ 05_web/mapper.py
@@ -0,0 +1,43 @@
+import contextlib
+import os
+import queue
+import requests
+import sys
+import threading
+import time
+
+FILTERED = [".jpg", ".gif", ".png", ".css"]
+TARGET = "http://127.0.0.1:31337/"
+THREADS = 10
+
+answers = queue.Queue()
+web_paths = queue.Queue()
+
+def gather_paths():
+    for root, _, files in os.walk('.'):
+        for fname in files:
+            if os.path.splitext(fname)[1] in FILTERED:
+                continue
+            path = os.path.join(root, fname)
+            if path.startswith('.'):
+                path = path[1:]
+            print(path)
+            web_paths.put(path)
+
+@contextlib.contextmanager
+def chdir(path):
+    """
+    On enter, change directory to specified path.
+    On exit, change directory back to original.
+    """
+    this_dir = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(this_dir)
+
+if __name__ == '__main__':
+    with chdir("/home/joh/Downloads/wordpress/"):
+        gather_paths()
+    input('Press return to continue.')
